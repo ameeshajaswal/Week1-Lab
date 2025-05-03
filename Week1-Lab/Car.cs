482b682499a9e3f3d09fc8abdb0dbb143f346a9d
dell <ameeshajaswal@gmail.com>
2025-05-03 18:24:19 -0400

    Add project files.

---
+++ Week1-Lab/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_Lab
+{
+    internal class Car
+    {
+        //Fields or attributes in car class
+        private int year;
+        private string model;
+        private bool isDrivable;
+        private double price;
+        //constructor with arguments
+        public Car(int year,string model,double price, bool isDrivable = true)
+        {
+            this.year = year;
+            this.model = model;
+            this.price = price;
+            this.isDrivable = isDrivable;
+        }
+        //using ToString method for display
+        public override string ToString()
+        {
+            return $"The car is of year :{year}, model :{model}, price :{price} and is Drivable";
+        }
+        //To get input from user about cars
+        public void getDetails()
+        {
+            Console.WriteLine("Enter the year of car Launch:");
+            this.year = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Model of car:");
+            this.model = Console.ReadLine();
+            Console.WriteLine("Enter the price of car:");
+            this.price = double.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Week1-Lab/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constructor calling
+            Car car1 = new Car(2022, "Civic", 50000, true);
+            Car car2 = new Car(2023, "Camrey", 60000, true);
+            Car car3 = new Car(2024, "3Series", 70000, true);
+            Car car4 = new Car(2025, "Truck", 80000, true);
+            Car car5 = new Car(2025, "Sedan", 70000, true);
+            //displaying cars
+            Console.WriteLine(car1.ToString());
+            Console.WriteLine(car2.ToString());
+            Console.WriteLine(car3.ToString());
+            Console.WriteLine(car4.ToString());
+            Console.WriteLine(car5.ToString());
+            //To get input of car details from user
+            car1.getDetails();
+            Console.WriteLine(car1.ToString());
+            car2.getDetails();
+            Console.WriteLine(car2.ToString());
+            car3.getDetails();
+            Console.WriteLine(car3.ToString());
+            car4.getDetails();
+            Console.WriteLine(car4.ToString());
+            car5.getDetails();
+            Console.WriteLine(car5.ToString());
+
+
+        }
+    }
+}
